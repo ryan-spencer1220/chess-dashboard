c60ac0710c752c266cbe0a49c0cb2dddfd4cc278
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-10-23 16:44:18 -0700

    add UserItem component

--- src/components/UserResults.jsx
+++ src/components/UserResults.jsx
@@ -1,30 +1,9 @@
-// import Spinner from "../components/assets/Spinner";
-// import UserItem from "../users/UserItem";
+import UserItem from "../users/UserItem";
 
-function UserResults() {
-  // if (!loading) {
-  //   return (
+function UserResults({ user }) {
   <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
-    {/* {users.map((user) => (
-          <UserItem key={user.id} user={user} />
-        ))} */}
-    <p>dogs</p>
-    <p>dogs</p>
-    <p>dogs</p>
-    <p>dogs</p>
-    <p>dogs</p>
-    <p>dogs</p>
-    <p>dogs</p>
-    <p>dogs</p>
-    <p>dogs</p>
-    <p>dogs</p>
-    <p>dogs</p>
-    <p>dogs</p>
+    <UserItem key={user.id} user={user} />
   </div>;
-  //     );
-  //   } else {
-  //     return <Spinner />;
-  //   }
 }
 
 export default UserResults;
