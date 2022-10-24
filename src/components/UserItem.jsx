45215caa069c935c8ec6b5d682cdccebcbda6caf
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-10-23 17:10:02 -0700

    add functionality for results to show on homepage

--- src/components/UserItem.jsx
+++ src/components/UserItem.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-function UserItem({ user: { login, avatar_url } }) {
+function UserItem({ user: { login, avatar } }) {
   return (
     <div className="card shadow-md compact side bg-base-100">
       <div className="flex-row items-center space-x-4 card-body">
         <div>
           <div className="avatar">
             <div className="rounded-full shadow w-14 h-14">
-              <img src={avatar_url} alt="Profile" />
+              <img src={avatar} alt="Profile" />
             </div>
           </div>
         </div>
--- src/components/UserResults.jsx
+++ src/components/UserResults.jsx
@@ -1,9 +1,20 @@
-import UserItem from "../users/UserItem";
+import { useContext } from "react";
+import { ChessContext } from "../context/ChessContext";
+import UserItem from "./UserItem";
 
-function UserResults({ user }) {
-  <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
-    <UserItem key={user.id} user={user} />
-  </div>;
+function UserResults() {
+  const { userData } = useContext(ChessContext);
+
+  if (userData) {
+    return (
+      <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+        <UserItem user={userData} />
+        {console.log("Hello1", userData)}
+      </div>
+    );
+  } else {
+    return <p>Dogs</p>;
+  }
 }
 
 export default UserResults;
