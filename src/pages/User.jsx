e2b263a50e3a7c1d56f713313e4941a24b06ade6
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-10-29 07:16:36 -0700

    add error handling for user stats

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -85,48 +85,60 @@ function User() {
             </div>
           </div>
         </div>
-
         <div className="w-full rounded-lg shadow-md bg-base-100 stats">
           <div className="grid grid-cols-1 md:grid-cols-3">
-            <div className="stat">
-              <div className="stat-figure text-secondary">
-                <FaUsers className="text-3xl md:text-5xl" />
+            {userStats.chess_blitz.last.rating ? (
+              <div className="stat">
+                <div className="stat-figure text-secondary">
+                  <FaUsers className="text-3xl md:text-5xl" />
+                </div>
+                <div className="stat-title">Blitz</div>
+                <div className="stat-value text-3xl md:text-4xl">
+                  {userStats.chess_blitz.last.rating}
+                </div>
               </div>
-              <div className="stat-title">Blitz</div>
-              <div className="stat-value text-3xl md:text-4xl">
-                {userStats.chess_blitz.last.rating}
+            ) : (
+              <></>
+            )}
+            {userStats.chess_rapid.last.rating ? (
+              <div className="stat">
+                <div className="stat-figure text-secondary">
+                  <MdOutlineCardMembership className="text-3xl md:text-5xl" />
+                </div>
+                <div className="stat-title">Rapid</div>
+                <div className="stat-value text-3xl md:text-4xl">
+                  {userStats.chess_rapid.last.rating}
+                </div>
               </div>
-            </div>
-
-            <div className="stat">
-              <div className="stat-figure text-secondary">
-                <MdOutlineCardMembership className="text-3xl md:text-5xl" />
+            ) : (
+              <></>
+            )}
+            {userStats.chess_bullet.last.rating ? (
+              <div className="stat">
+                <div className="stat-figure text-secondary">
+                  <AiFillCrown className="text-3xl md:text-5xl" />
+                </div>
+                <div className="stat-title">Bullet</div>
+                <div className="stat-value text-3xl md:text-4xl">
+                  {userStats.chess_bullet.last.rating}
+                </div>
               </div>
-              <div className="stat-title">Rapid</div>
-              <div className="stat-value text-3xl md:text-4xl">
-                {userStats.chess_rapid.last.rating}
+            ) : (
+              <></>
+            )}
+            {userStats.chess_daily.last.rating ? (
+              <div className="stat">
+                <div className="stat-figure text-secondary">
+                  <AiFillCrown className="text-3xl md:text-5xl" />
+                </div>
+                <div className="stat-title">Daily</div>
+                <div className="stat-value text-3xl md:text-4xl">
+                  {userStats.chess_daily.last.rating}
+                </div>
               </div>
-            </div>
-
-            <div className="stat">
-              <div className="stat-figure text-secondary">
-                <AiFillCrown className="text-3xl md:text-5xl" />
-              </div>
-              <div className="stat-title">Bullet</div>
-              <div className="stat-value text-3xl md:text-4xl">
-                {userStats.chess_bullet.last.rating}
-              </div>
-            </div>
-
-            <div className="stat">
-              <div className="stat-figure text-secondary">
-                <AiFillCrown className="text-3xl md:text-5xl" />
-              </div>
-              <div className="stat-title">Daily</div>
-              <div className="stat-value text-3xl md:text-4xl">
-                {userStats.chess_daily.last.rating}
-              </div>
-            </div>
+            ) : (
+              <></>
+            )}
           </div>
         </div>
       </div>
