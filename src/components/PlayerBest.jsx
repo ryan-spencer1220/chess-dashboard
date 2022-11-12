7d969d601468c259bbabc305f42c254a97864e48
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-11-12 09:55:33 -0800

    add progress section

--- src/components/PlayerBest.jsx
+++ src/components/PlayerBest.jsx
@@ -9,66 +9,33 @@ import { BsLightningFill, BsFillStopwatchFill } from "react-icons/bs";
 import { GiBulletBill } from "react-icons/gi";
 
 const PlayerRating = () => {
-  const { userStats, userData } = useContext(ChessContext);
-
-  console.log(userStats);
-
+  const { userStats } = useContext(ChessContext);
   return (
-    <div className="w-full rounded-lg shadow-md bg-base-100 stats">
-      <div className="grid grid-cols-1 md:grid-cols-4">
-        {userStats.chess_blitz.best.rating ? (
-          <div className="stat">
-            <div className="stat-figure text-secondary">
-              <BsLightningFill className="text-3xl md:text-5xl" />
-            </div>
-            <div className="stat-title">Blitz (best)</div>
-            <div className="stat-value text-3xl md:text-4xl">
-              {userStats.chess_blitz.best.rating}
-            </div>
-          </div>
-        ) : (
-          <>N/A</>
-        )}
-        {userStats.chess_rapid.best.rating ? (
-          <div className="stat">
-            <div className="stat-figure text-secondary">
-              <BsFillStopwatchFill className="text-3xl md:text-5xl" />
-            </div>
-            <div className="stat-title">Rapid (best)</div>
-            <div className="stat-value text-3xl md:text-4xl">
-              {userStats.chess_rapid.best.rating}
-            </div>
-          </div>
-        ) : (
-          <>N/A</>
-        )}
-        {userStats.chess_bullet.best.rating ? (
-          <div className="stat">
-            <div className="stat-figure text-secondary">
-              <GiBulletBill className="text-3xl md:text-5xl" />
-            </div>
-            <div className="stat-title">Bullet (best)</div>
-            <div className="stat-value text-3xl md:text-4xl">
-              {userStats.chess_bullet.best.rating}
-            </div>
-          </div>
-        ) : (
-          <>N/A</>
-        )}
-        {userStats.chess_daily.best.rating ? (
-          <div className="stat">
-            <div className="stat-figure text-secondary">
-              <TiWeatherSunny className="text-3xl md:text-5xl" />
-            </div>
-            <div className="stat-title">Daily (best)</div>
-            <div className="stat-value text-3xl md:text-4xl">
-              {userStats.chess_daily.best.rating}
-            </div>
-          </div>
-        ) : (
-          <>N/A</>
-        )}
-      </div>
+    <div className="flex flex-col rounded-lg shadow-md bg-base-100 stats p-10 w-auto stat-value">
+      Blitz
+      <progress
+        className="progress progress-primary w-full h-5 m-2"
+        value={userStats.chess_blitz.last.rating}
+        max={userStats.chess_blitz.best.rating}
+      ></progress>
+      Rapid
+      <progress
+        className="progress progress-primary w-full h-5 m-2"
+        value={userStats.chess_blitz.last.rating}
+        max={userStats.chess_rapid.best.rating}
+      ></progress>
+      Bullet
+      <progress
+        className="progress progress-primary w-full h-5 m-2"
+        value={userStats.chess_blitz.last.rating}
+        max={userStats.chess_bullet.best.rating}
+      ></progress>
+      Daily
+      <progress
+        className="progress progress-primary w-full h-5 m-2"
+        value={userStats.chess_blitz.last.rating}
+        max={userStats.chess_daily.best.rating}
+      ></progress>
     </div>
   );
 };
